3e18786a70d6f09b1769825cb71e3a7171f8b41d
Hikmat Samadov <s3m3dov@hotmail.com>
2023-08-18 15:37:55 +0400

    refactor: update test

--- test.py
+++ test.py
@@ -2,26 +2,29 @@ import logging
 import os
 from platform import python_version
 
-from log_to_slack import SlackLogHandler, NoStacktraceFormatter
+from log_to_slack import SlackLogHandler
+from log_to_slack.formatters import NoStacktraceFormatter, DefaultFormatter
 
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
-slack_handler = SlackLogHandler(
-    webhook_url=WEBHOOK_URL,
-    stack_trace=True,
-)
-slack_handler.setLevel(logging.WARNING)
-
 logger = logging.getLogger("debug_application")
-logger.addHandler(logging.StreamHandler())
-logger.addHandler(slack_handler)
 logger.setLevel(logging.DEBUG)
 
-formatter = NoStacktraceFormatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-slack_handler.setFormatter(formatter)
+default_formatter = DefaultFormatter("%(levelprefix)s %(asctime)s (%(name)s) %(message)s")
+no_stacktrace_formatter = NoStacktraceFormatter("%(levelprefix)s %(asctime)s (%(name)s) %(message)s")
 
+default_handler = logging.StreamHandler()
+default_handler.setFormatter(default_formatter)
+logger.addHandler(default_handler)
+
+slack_handler = SlackLogHandler(webhook_url=WEBHOOK_URL, stack_trace=True)
+slack_handler.setFormatter(no_stacktrace_formatter)
+slack_handler.setLevel(logging.ERROR)
+logger.addHandler(slack_handler)
+
+slack_handler_2 = SlackLogHandler(webhook_url=WEBHOOK_URL, stack_trace=True, traceback_len=8000, msg_len=-1)
+
+# Main code
 logger.info("Python version is {}".format(python_version()))
 
 logger.debug("Test DEBUG")
